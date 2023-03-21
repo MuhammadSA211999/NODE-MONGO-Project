041f7aace02a99547312fc13641b7a3726e9343f
Muhammad SA <MuhammadSA211999>
2023-03-21 20:14:15 +0600

    locally conncet mongodb with mongoose

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
 app.use(express.json())
 app.use(cors())
-const PORT = process.env.PORT
+const PORT = 9000
 
+//connect database with mongoose 
+mongoose.connect(`mongodb://localhost:27017/todo`, {
+    //options
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+}).then(() => {
+    console.log(`successfully connected to mongodb://localhost:27017/todo`);
+
+}).catch(err => {
+    console.log(`connection error=>`, err);
+
+})
 app.get('/', (req, res) => {
     res.json({ message: 'success', data: 'Todo app home route' })
 })
-app.listen(PointerEvent, () => {
+
+const appErrorHandler = (err, req, res, next) => {
+    console.log(err);
+    next()
+}
+app.listen(PORT, () => {
     console.log(`todo app are running on port ${PORT}`);
 })
